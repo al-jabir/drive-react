63f02ddeaa33f5724ebf98c01df4e9d30ad9fce3
al-jabir <aljabir098@gmail.com>
2022-09-20 21:31:28 +0600

    some add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineUser } from "react-icons/ai";
+import Logo from "../../Images/logo.png";
 import styled from "./navbar.module.css";
 const Navbar = () => {
   return (
     <header className={styled.header}>
-      <ul className={styled.menu}>
-        <li>
-          <a href="/#">Learn More</a>
-        </li>
-        <li>
-          <a href="/#">Log in</a>
-        </li>
-        <li>
-          <a href="/#">Sign up</a>
-        </li>
-        <li>
-          <AiOutlineSearch />
-        </li>
-        <li>
-          <AiOutlineUser />
-        </li>
-        <li>
-          <AiOutlineMenu />
-        </li>
-      </ul>
+      <img src={Logo} alt="/" />
+      <nav>
+        <ul className={styled.menu}>
+          <li>
+            <a href="/#">Learn More</a>
+          </li>
+          <li>
+            <a href="/#">Log in</a>
+          </li>
+          <li>
+            <a href="/#">Sign up</a>
+          </li>
+          <li>
+            <AiOutlineSearch size={25} style={{ marginTop: "6px" }} />
+          </li>
+          <li>
+            <AiOutlineUser size={25} style={{ marginTop: "6px" }} />
+          </li>
+        </ul>
+      </nav>
+      <div className={styled.btn_mobile}>
+        <AiOutlineMenu />
+      </div>
     </header>
   );
 };
