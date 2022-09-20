495a271412599600afa3f1ed081c6008a1572c5f
al-jabir <aljabir098@gmail.com>
2022-09-20 21:58:31 +0600

    I have finsih Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { AiOutlineMenu, AiOutlineSearch, AiOutlineUser } from "react-icons/ai";
+import React, { useState } from "react";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineUser,
+} from "react-icons/ai";
 import Logo from "../../Images/logo.png";
 import styled from "./navbar.module.css";
 const Navbar = () => {
+  const [bar, setBar] = useState(false);
   return (
     <header className={styled.header}>
       <img src={Logo} alt="/" />
       <nav>
-        <ul className={styled.menu}>
+        <ul
+          className={
+            bar ? [styled.menu, styled.active].join(" ") : [styled.menu]
+          }
+        >
           <li>
             <a href="/#">Learn More</a>
           </li>
@@ -25,8 +35,8 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
-      <div className={styled.btn_mobile}>
-        <AiOutlineMenu />
+      <div className={styled.btn_mobile} onClick={() => setBar(!bar)}>
+        {bar ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
       </div>
     </header>
   );
